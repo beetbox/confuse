49d4d72da90459c3c8c75ff8632df01b9eb9fc31
Adrian Sampson <adrian@radbox.org>
2012-05-10 17:14:28 -0700

    basic Sphinx docs (README is getting unwieldy)

---
+++ docs/conf.py
@@ -0,0 +1,19 @@
+extensions = []
+source_suffix = '.rst'
+master_doc = 'index'
+
+project = u'Confit'
+copyright = u'2012, Adrian Sampson'
+
+version = '0.1'
+release = '0.1.0'
+
+exclude_patterns = ['_build']
+
+pygments_style = 'sphinx'
+
+# -- Options for HTML output ---------------------------------------------------
+
+html_theme = 'default'
+html_static_path = ['_static']
+htmlhelp_basename = 'Confitdoc'
