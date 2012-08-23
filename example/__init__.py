64b77014d2a2ef5a285237c2f49c96e6613678b6
Adrian Sampson <adrian@radbox.org>
2012-08-22 20:12:48 -0700

    start fleshing out example: a skeleton for beets

--- example/__init__.py
+++ example/__init__.py
@@ -7,9 +7,18 @@ import argparse
 def main():
     config = confit.config('ConfitExample', __name__)
 
-    parser = argparse.ArgumentParser(description='test program for Confit')
-    parser.add_argument('--foo', dest='foo', metavar='BAR',
-                        help='a parameter')
+    parser = argparse.ArgumentParser(description='example Confit program')
+    parser.add_argument('--library', '-l', dest='library', metavar='LIBPATH',
+                        help='library database file')
+    parser.add_argument('--directory', '-d', dest='directory',
+                        metavar='DIRECTORY',
+                        help='destination music directory')
+    parser.add_argument('--verbose', '-v', dest='verbose', action='store_true',
+                        help='print debugging messages')
 
     parser.parse_args(namespace=config.arg_namespace)
-    print(config['foo'].get())
+
+    if config['verbose'].get(bool):
+        print('verbose mode')
+    print('directory is', config['directory'].get(confit.as_filename))
+    print('library is', config['library'].get(confit.as_filename))
