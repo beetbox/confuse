d5cf8a66fa19a9cab3f3ea778877b910a0489fc6
Adrian Sampson <adrian@radbox.org>
2015-12-05 06:22:34 +0100

    Change PyPI name to `confer` (#18)

--- setup.py
+++ setup.py
@@ -2,32 +2,36 @@ from setuptools import setup
 import os
 import sys
 
+
 def _read(fn):
     path = os.path.join(os.path.dirname(__file__), fn)
     return open(path).read()
+
 
 reqs = ['pyyaml']
 if sys.version_info[0] <= 2 and sys.version_info[1] < 7:
     reqs.append('ordereddict')
 
-setup(name='confit',
-      version='0.1.0',
-      description='painless YAML configuration',
-      author='Adrian Sampson',
-      author_email='adrian@radbox.org',
-      url='https://github.com/sampsyo/confit',
-      license='MIT',
-      platforms='ALL',
-      long_description=_read("README.rst"),
 
-      install_requires=reqs,
+setup(
+    name='confer',
+    version='0.2.0',
+    description='painless YAML configuration',
+    author='Adrian Sampson',
+    author_email='adrian@radbox.org',
+    url='https://github.com/sampsyo/confit',
+    license='MIT',
+    platforms='ALL',
+    long_description=_read("README.rst"),
 
-      py_modules=['confit'],
+    install_requires=reqs,
 
-      classifiers=[
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 3',
-      ],
+    py_modules=['confit'],
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
 )
