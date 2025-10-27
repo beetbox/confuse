cb0ef542517dc12fd74e505a9d7b2ab1ac3adaaa
Šarūnas Nejus <snejus@protonmail.com>
2025-10-27 11:06:19 +0000

    Reformat docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,33 +1,30 @@
+import datetime as dt
 import os
 import sys
-import datetime as dt
-
 
 sys.path.insert(0, os.path.abspath(".."))
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.autosectionlabel',
-    'sphinx_rtd_theme',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autosectionlabel",
+    "sphinx_rtd_theme",
 ]
-source_suffix = '.rst'
-master_doc = 'index'
+source_suffix = ".rst"
+master_doc = "index"
 
-project = u'Confuse'
-copyright = '2012-{}, Adrian Sampson & contributors'.format(
-    dt.date.today().year
-)
+project = "Confuse"
+copyright = "2012-{}, Adrian Sampson & contributors".format(dt.date.today().year)
 
-version = '1.7'
-release = '1.7.0'
+version = "2.1"
+release = "2.1.1"
 
-exclude_patterns = ['_build']
+exclude_patterns = ["_build"]
 
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # -- Options for HTML output --------------------------------------------------
 
-html_theme = 'sphinx_rtd_theme'
-htmlhelp_basename = 'Confusedoc'
+html_theme = "sphinx_rtd_theme"
+htmlhelp_basename = "Confusedoc"
